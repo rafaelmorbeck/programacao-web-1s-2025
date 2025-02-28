0ec3c8531b5e8e245be44b055de9ba4aee6fcdef
rafaelmorbeck <123705052+rafaelmorbeck@users.noreply.github.com>
2025-02-28 10:43:44 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let x = 5
+let y = 2
+let resultado;
+
+//soma
+resultado = x + y;
+console.log(`Resultado da soma entre ${x} e ${y} é ${resultado}`);
+
+//subtracao
+resultado = x - y;
+console.log(`Resultado da subtracao entre ${x} e ${y} é ${resultado}`)
+
+//multiplicacao
+resultado = x * y;
+console.log(`Resultado da multiplicacao entre ${x} e ${y} é ${resultado}`)
+
+//divisao
+resultado = x/y;
+console.log(`Resultado da divisao entre ${x} e ${y} é ${resultado}`)
