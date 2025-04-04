47f3162dbcf2409037668bb0436b33fa05ef3f4c
rafaelmorbeck <123705052+rafaelmorbeck@users.noreply.github.com>
2025-04-04 09:56:28 -0300

    Add files via upload

---
+++ estoque.js
@@ -0,0 +1,71 @@
+let itens = [];
+
+function adicionar(item){
+    let item_valido = validar_item(item);
+    
+    if(item_valido){
+        itens.push(item);   //adicionar cada item que esta sendo requisitado
+    }
+    return item_valido
+}
+
+function listar(){
+    return itens;
+}
+
+function editar(id,qntd){
+    if(!is_numerico(id) || !is_id_cadastrado(id) || !is_numerico(qntd) || qntd < 0){
+        return false;
+    }
+    itens.forEach(item_cadastrado=>{
+        if(item_cadastrado.id == id){
+            item_cadastrado.qntd = qntd;
+        }
+    });
+    return true
+}
+
+function is_numerico(n){
+    if(isNaN(item.n) || item.n == null){
+        return false;
+    }
+    return true;
+}
+
+function is_id_cadastrado(id){
+    let item_existente = false;
+    itens.forEach(item_cadastrado =>{
+        if(item.id == item_cadastrado.id){
+            item_existente = true;
+        }
+        return item_existente;
+    });
+}
+
+function validar_item(item){
+    let item_valido = true;
+
+
+    if(is_numerico(item.id) || item.id <= 0){
+        item_valido = false;
+    }
+
+    if (is_id_cadastrado(item.id)){
+        item_valido = false;
+    }
+
+    if(item.nome.length){
+        item_valido = false;
+    }
+
+    if(!is_numerico(item.qntd) || item.qntd < 0){
+        item_valido = false;
+    }
+    
+
+    return item_valido;
+}
+
+module.exports={
+    adicionar, listar, editar
+}
